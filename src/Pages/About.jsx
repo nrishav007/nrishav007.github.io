82a7b15937cb09a84801289b01f4e4e873b1240d
Rishav Neogi <nrishav31@gmail.com>
2022-12-13 16:28:48 +0530

    UI modified

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
       <Box paddingTop={"30px"} backgroundColor={"blue.700"} color="white"  h={"full"} paddingBottom={"50px"}>
         <Text fontSize={["10px","20px","20px","35px"]} marginBottom="50px">About Me</Text>
         <Center>
-          <Text w={["95%","95%","50%","60%"]} fontSize={["10px","15px","15px","25px"]} lineHeight={"50px"}>A passionate Full Stack Web Developer, Dedicated to developing robust applications that interact with the Frontend part of web applications. Extensive expertise in website architecture, design, and web design, and web API designs. Problem-solving mindset with a goal to optimize my application for better stability and speed. Love to work in a team.</Text>
+          <Text w={["95%","95%","50%","60%"]} fontSize={["10px","15px","15px","20px"]} lineHeight={"50px"}>A passionate Full Stack Web Developer, Dedicated to developing robust applications that interact with the Frontend part of web applications. Extensive expertise in website architecture, design, and web design, and web API designs. Problem-solving mindset with a goal to optimize my application for better stability and speed. Love to work in a team.</Text>
         </Center>
       </Box>
     </Box>
