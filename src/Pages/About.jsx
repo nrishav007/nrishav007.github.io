8162b11e0d65c7faf55ee432de9ff891ba000a81
Rishav Neogi <nrishav31@gmail.com>
2022-12-19 15:31:01 +0530

    about modified

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 
 const About = () => {
   return (
-    <Box  id='About Me' h={"500px"}>
-      <Box paddingTop={"30px"} backgroundColor={"blue.700"} color="white"  h={"full"} paddingBottom={"50px"}>
+    <Box  id='About Me' h={"400px"}>
+      <Box paddingTop={"10px"} backgroundColor={"blue.700"} color="white"  h={"full"} paddingBottom={"50px"}>
         <Text fontSize={["10px","20px","20px","35px"]} marginBottom="50px">About Me</Text>
         <Center>
           <Text w={["95%","95%","50%","60%"]} fontSize={["10px","15px","15px","20px"]} lineHeight={"50px"}>A passionate Full Stack Web Developer, Dedicated to developing robust applications that interact with the Frontend part of web applications. Extensive expertise in website architecture, design, and web design, and web API designs. Problem-solving mindset with a goal to optimize my application for better stability and speed. Love to work in a team.</Text>
