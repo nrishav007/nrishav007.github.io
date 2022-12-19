0a0bb078814cfba6851217c84c7a9f83d657dacb
Rishav Neogi <nrishav31@gmail.com>
2022-12-19 13:57:43 +0530

    font family added

--- src/App.js
+++ src/App.js
@@ -1,35 +1,36 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import { Desktop } from './Components/Navbar/Desktop';
-import Mobile from './Components/Navbar/Mobile';
-import { Home } from './Pages/Home';
-import { About } from './Pages/About';
-import { Skills } from './Pages/Skills';
-import { Projects } from './Pages/Projects';
-import { Contact } from './Pages/Contact';
-import Git from './Pages/Git';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { Desktop } from "./Components/Navbar/Desktop";
+import Mobile from "./Components/Navbar/Mobile";
+import { Home } from "./Pages/Home";
+import { About } from "./Pages/About";
+import { Skills } from "./Pages/Skills";
+import { Projects } from "./Pages/Projects";
+import { Contact } from "./Pages/Contact";
+import Git from "./Pages/Git";
+import { Box } from "@chakra-ui/react";
+import Font from "react-font";
 function App() {
   document.title = "Rishav's Portfolio";
 
   const [size, setSize] = useState(window.innerWidth);
 
-  const updateSize = () =>
-    setSize(window.innerWidth);
+  const updateSize = () => setSize(window.innerWidth);
 
   useEffect(() => (window.onresize = updateSize), []);
 
   return (
-    <div className="App">
-      {
-        size >= 525 ? <Desktop /> : <Mobile />
-      }
-      <Home/>
-      <About />
-      <Skills />
-      <Projects />
-      <Git />
-      <Contact />
-    </div>
+    <Box className="App">
+      <Font family='Ultra' weight={600}>
+        {size >= 525 ? <Desktop /> : <Mobile />}
+        <Home />
+        <About />
+        <Skills />
+        <Projects />
+        <Git />
+        <Contact />
+      </Font>
+    </Box>
   );
 }
 
