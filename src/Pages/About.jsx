b5358a84e3bd4e7a62caedf8a593be179ed70df1
nrishav007 <nrishav31@gmail.com>
2024-03-20 18:01:00 +0530

    about section updated

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
       <Box paddingTop={"10px"} h={"full"} paddingBottom={"50px"}>
         <Text fontWeight={"bold"} fontSize={["10px","20px","20px","35px"]} marginBottom="50px">About Me</Text>
         <Center>
-          <Text w={["95%","95%","50%","60%"]} fontSize={["10px","15px","15px","22px"]} lineHeight={"40px"}>Web developer with a strong focus on React. Proficient in HTML, CSS, JavaScript, MongoDB, Express, and node. Skilled in creating responsive, user-friendly websites with a strong emphasis on design. Proven ability to adapt to new technologies and collaborate effectively in a team environment.</Text>
+          <Text w={["95%","95%","50%","60%"]} fontSize={["10px","15px","15px","22px"]} lineHeight={"40px"}>As a dedicated MERN (MongoDB, Express.js, React.js, Node.js) stack developer with one year of professional experience, I have honed my skills in developing robust and scalable web applications. My journey in the MERN stack has equipped me with a comprehensive understanding of both frontend and backend development, allowing me to seamlessly integrate features and functionalities across the stack.</Text>
         </Center>
       </Box>
     </Box>
